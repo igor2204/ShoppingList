6d61d4a370734e048b5c0878ef6daa7e102adf51
Цомкалов Игорь <igorcomkalov@gmail.com>
2024-06-03 09:53:39 +0300

    все

--- ShoppingList/Form1.Designer.cs
+++ ShoppingList/Form1.Designer.cs
@@ -40,70 +40,73 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.labelTotalCost = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(276, 171);
+            this.button1.Location = new System.Drawing.Point(25, 171);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(139, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.buttonAddItem_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(387, 171);
+            this.button3.Location = new System.Drawing.Point(184, 171);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(139, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Удалить товар";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.buttonRemoveItem_Click);
             // 
             // textBoxItem
             // 
-            this.textBoxItem.Location = new System.Drawing.Point(488, 52);
+            this.textBoxItem.Location = new System.Drawing.Point(301, 20);
             this.textBoxItem.Multiline = true;
             this.textBoxItem.Name = "textBoxItem";
-            this.textBoxItem.Size = new System.Drawing.Size(100, 24);
+            this.textBoxItem.Size = new System.Drawing.Size(172, 24);
             this.textBoxItem.TabIndex = 3;
             this.textBoxItem.TextChanged += new System.EventHandler(this.textBoxItem_TextChanged);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(488, 171);
+            this.button4.Location = new System.Drawing.Point(334, 171);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(139, 23);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "Удалить все";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.buttonClearList_Click);
             // 
             // listBoxItems
             // 
             this.listBoxItems.FormattingEnabled = true;
-            this.listBoxItems.Location = new System.Drawing.Point(314, 52);
+            this.listBoxItems.Location = new System.Drawing.Point(25, 13);
             this.listBoxItems.Name = "listBoxItems";
-            this.listBoxItems.Size = new System.Drawing.Size(120, 95);
+            this.listBoxItems.Size = new System.Drawing.Size(193, 134);
             this.listBoxItems.TabIndex = 5;
             this.listBoxItems.SelectedIndexChanged += new System.EventHandler(this.listBoxItems_SelectedIndexChanged);
             // 
             // textBoxPrice
             // 
-            this.textBoxPrice.Location = new System.Drawing.Point(488, 106);
+            this.textBoxPrice.Location = new System.Drawing.Point(301, 92);
             this.textBoxPrice.Multiline = true;
             this.textBoxPrice.Name = "textBoxPrice";
-            this.textBoxPrice.Size = new System.Drawing.Size(100, 24);
+            this.textBoxPrice.Size = new System.Drawing.Size(172, 24);
             this.textBoxPrice.TabIndex = 6;
             // 
             // textBoxQuantity
             // 
-            this.textBoxQuantity.Location = new System.Drawing.Point(622, 52);
+            this.textBoxQuantity.Location = new System.Drawing.Point(301, 53);
             this.textBoxQuantity.Multiline = true;
             this.textBoxQuantity.Name = "textBoxQuantity";
-            this.textBoxQuantity.Size = new System.Drawing.Size(100, 24);
+            this.textBoxQuantity.Size = new System.Drawing.Size(172, 24);
             this.textBoxQuantity.TabIndex = 7;
             // 
             // openFileDialog1
@@ -112,38 +115,68 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(599, 171);
+            this.button2.Location = new System.Drawing.Point(99, 200);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(139, 23);
             this.button2.TabIndex = 8;
-            this.button2.Text = "button2";
+            this.button2.Text = "Сохранить файл";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(699, 171);
+            this.button5.Location = new System.Drawing.Point(261, 200);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(139, 23);
             this.button5.TabIndex = 9;
-            this.button5.Text = "button5";
+            this.button5.Text = "Открыть файл";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.buttonLoad_Click);
             // 
             // labelTotalCost
             // 
             this.labelTotalCost.AutoSize = true;
-            this.labelTotalCost.Location = new System.Drawing.Point(658, 134);
+            this.labelTotalCost.Location = new System.Drawing.Point(224, 134);
             this.labelTotalCost.Name = "labelTotalCost";
-            this.labelTotalCost.Size = new System.Drawing.Size(35, 13);
+            this.labelTotalCost.Size = new System.Drawing.Size(99, 13);
             this.labelTotalCost.TabIndex = 10;
-            this.labelTotalCost.Text = "label1";
+            this.labelTotalCost.Text = "Общая стоимость";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Название";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Количество";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Цена";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(511, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelTotalCost);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button2);
@@ -176,6 +209,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label labelTotalCost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
